3a98dceefe4641657040b4423b3990e0da98eb45
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-04-12 12:11:17 +0300

    1

---
+++ js/slick.js
@@ -0,0 +1,18 @@
+import Swiper from 'swiper';
+import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
+$(function () {
+  $(".class-gallery-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".class-gallery-list-left",
+    nextArrow: ".class-gallery-list-right",
+  });
+});
